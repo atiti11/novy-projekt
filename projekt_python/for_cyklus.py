172cd4871f742521d79fa3a4ac42755fc6063d73
atiti1 <kmentova.anna@student.alej.cz>
2024-08-22 19:22:03 +0200

    přidávám podtržení

--- projekt_python/for_cyklus.py
+++ projekt_python/for_cyklus.py
@@ -3,10 +3,11 @@ students = [
     {"jméno": "Petra", "příjmení": "Svobodová", "věk": 22},
     {"jméno": "Lukáš", "příjmení": "Dvořák", "věk": 19},
     {"jméno": "Eva", "příjmení": "Kučerová", "věk": 21},
-    {"jméno": "Tomáš", "příjmení": "Král", "věk": 23}
+    {"jméno": "Tomáš", "příjmení": "Král", "věk": 23},
 ]
 
 for student in students:
-    print("jméno:", student["jméno"], end= ", ")
-    print("příjmení:", student["příjmení"], end= ", ")
+    print("jméno:", student["jméno"], end=", ")
+    print("příjmení:", student["příjmení"], end=", ")
     print("věk:", student["věk"])
+    print("---------")
