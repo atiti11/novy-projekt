e9da86da4226cc13089cd18446db26fe23e82ea4
atiti11 <100147119+atiti11@users.noreply.github.com>
2024-08-22 18:14:23 +0200

    přidávám soubor

---
+++ projekt_python/for_cyklus.py
@@ -0,0 +1,14 @@
+students = [
+    {"jméno": "Jan", "příjmení": "Novák", "věk": 20},
+    {"jméno": "Petra", "příjmení": "Svobodová", "věk": 22},
+    {"jméno": "Lukáš", "příjmení": "Dvořák", "věk": 19},
+    {"jméno": "Eva", "příjmení": "Kučerová", "věk": 21},
+    {"jméno": "Tomáš", "příjmení": "Král", "věk": 23}
+]
+
+students.reverse()
+
+for student in students:
+    print("jméno:", student["jméno"], end= ", ")
+    print("příjmení:", student["příjmení"], end= ", ")
+    print("věk:", student["věk"])
